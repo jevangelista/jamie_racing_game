c41b0407936caaa744e201f02cd5c97aafa87b5e
Jamie Eula Evangelista <jevangelista@Jamies-MBP.att.net>
2015-08-15 11:56:17 -0700

    Added varCounterPlayerTwo to get the players moving separately in their lanes

--- base.js
+++ base.js
@@ -1,24 +1,27 @@
 console.log("Sanity Check: JS is working!");
 
+//Initializes counter for playerOne and playerTwo
+var counterPlayerOne = 0;
+var counterPlayerTwo = 0; 
+
 //Function triggers a move when keyboard button is pressed
-var counter = 0;
 $(document).keypress(function(action){
 
 //Triggers playerOne to move when "1" is pressed
 	if (action.which == 49) {
 		var playerOne = $("#playerOne");
 		$("#playerOne").remove();
-		var $newBlock = $(".block").eq(counter + 1);
+		var $newBlock = $(".block").eq(counterPlayerOne + 1);
 		$newBlock.append(playerOne);
-		counter = (counter + 1)
+		counterPlayerOne = (counterPlayerOne + 1);
 
 //Triggers playerTwo to move when "0" is pressed		
 	} else if (action.which == 48) {
 		var playerTwo = $("#playerTwo");
 		$("#playerTwo").remove();
-		var $newBlock2 = $(".block2").eq(counter + 1);
+		var $newBlock2 = $(".block2").eq(counterPlayerTwo + 1);
 		$newBlock2.append(playerTwo);
-		counter = (counter + 1)
+		counterPlayerTwo = (counterPlayerTwo + 1)
 	}
 	
 })
