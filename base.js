c4600d08fb0e0d2ed548478ce264fcbcfe5dedf8
Jamie Eula Evangelista <jevangelista@Jamies-MBP.att.net>
2015-08-15 12:41:40 -0700

    Added conditional in js file to keep track of wins and say who won the round

--- base.js
+++ base.js
@@ -2,7 +2,10 @@ console.log("Sanity Check: JS is working!");
 
 //Initializes counter for playerOne and playerTwo
 var counterPlayerOne = 0;
-var counterPlayerTwo = 0; 
+var counterPlayerTwo = 0;
+var playerOneWins = 0;
+var playerTwoWins = 0;
+
 
 //Function triggers a move when keyboard button is pressed
 $(document).keypress(function(action){
@@ -23,5 +26,34 @@ $(document).keypress(function(action){
 		$newBlock2.append(playerTwo);
 		counterPlayerTwo = (counterPlayerTwo + 1)
 	}
+
+//Will keep track of wins and say who won the round
+	if (counterPlayerOne == 9) {
+		playerOneWins += 1;
+		$('h2').append(" " + "Player One wins!" + " Score: " + playerOneWins);
+		alert("You go Player One!");
+
+	} else if (counterPlayerTwo == 9) {
+		playerTwoWins += 1;
+		$('h2').append(" " + "Player Two wins!" + " Score: " + playerOneWins);
+		alert("You go Player Two!");
+	}
+
+
 	
 })
+
+/**
+console.log("Sanity Check: JS is working!");
+
+$(document).ready(function(){
+
+$("span").on("click", function handleClick(event){
+   var text = $(this).text();
+   console.log(text);
+   $('ul').append("<li>" + text + "</li>");
+   
+});
+
+})
+*/
